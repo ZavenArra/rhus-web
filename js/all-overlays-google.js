9ded51d9b386f3cd0ca334f55e89ee02e91d105d
Snacks <Armadillo@Armadillo.local>
2012-03-12 22:21:36 -0700

    killed the popup on the map

--- js/all-overlays-google.js
+++ js/all-overlays-google.js
@@ -7,11 +7,16 @@ function init() {
         allOverlays: true
     });
 
-    var osm = new OpenLayers.Layer.OSM();
-    var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: false});
-
+	
+		var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: true},
+				{'layers': 'basic'},
+				{'maxExtent': new OpenLayers.Bounds(-83.1,42.4,-83,42), 
+				'maxResolution': 156543,
+				units: 'meters'});
+		
+		var osm = new OpenLayers.Layer.OSM("Open Street Map", {visibilty:false});
     // note that first layer must be visible
-    map.addLayers([osm, gmap]);
+    map.addLayers([gmap, osm]);
 
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomToMaxExtent();
