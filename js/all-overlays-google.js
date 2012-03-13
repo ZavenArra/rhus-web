fdc99e297e42832ae4ef19c3d5b7415bee4be76f
Snacks <Armadillo@Armadillo.local>
2012-03-12 23:31:06 -0700

    working on the map loading

--- js/all-overlays-google.js
+++ js/all-overlays-google.js
@@ -2,23 +2,24 @@ var map;
 
 function init() {
 
-    map = new OpenLayers.Map({
-        div: "map",
-        allOverlays: true
-    });
+	map = new OpenLayers.Map({
+div: "map",
+allOverlays: true
+});
 
-		var osm = new OpenLayers.Layer.OSM("Open Street Map", {visibilty: true},
-				{'layers': 'basic'},
-				{'maxExtent': new OpenLayers.Bounds(42.385937, -82.946434, 42.379344, -83.266068),
-				'maxResolution': "auto"});
+var osm = new OpenLayers.Layer.OSM("Open Layers",                                            
+		null,
+		{'layers':'basic'},
+		{'maxExtent': new OpenLayers.Bounds(-83.266068,42.379344,-82.913132,42.417881), 
+		'maxResolution': "auto"});
 
+var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: false});
 
-		var gmap = new OpenLayers.Layer.Google("Google Streets", {visibility: true});
-		
-    // note that first layer must be visible
-    map.addLayers([ osm, gmap]);
+// note that first layer must be visible
+     map.addLayers([osm, gmap]);
 
     map.addControl(new OpenLayers.Control.LayerSwitcher());
     map.zoomToMaxExtent();
 
-}
+             }
+
