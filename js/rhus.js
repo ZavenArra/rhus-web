335d17feb8fda90fa520e40930e7567cdf67b96a
Deepwinter <deepwinter@winterroot.net>
2012-03-14 21:01:43 -0700

    Study area overlays added

--- js/rhus.js
+++ js/rhus.js
@@ -92,12 +92,227 @@ rhus.map = new Class({
 
     this.map = new OpenLayers.Map( 'map');
 
-    this.osm = new OpenLayers.Layer.OSM( "OSM Map Layer");
+    this.osm = new OpenLayers.Layer.OSM( "Open Street Map Layer");
     this.map.addLayer(this.osm);
+
+    /* defined for javascript.
+    if( google ){
+    alert(google);
+    var gmap = new OpenLayers.Layer.Google("Google Streets Layer", {visibility: false});
+    this.map.addLayer(gmap);
+    }
+    */
 
     //Add Other Layers
     //topo = new OpenLayers.Layer.OSM( "OSM Map Layer");
     //this.map.addLayer(topo);
+    
+    //Add Detroit Overlay
+    //And also for timelines...
+    //These need to be defined in separate files and loaded per implementation
+    //Detroit.js, Squirrels.js, etc.
+
+    var styles = new OpenLayers.StyleMap({
+      "default": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                pointRadius: 5,
+                graphicName: "square",
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 1,
+                strokeOpacity: 1,
+                strokeColor: "#3333aa"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#6666aa"
+              },
+              "Polygon": {
+                strokeWidth: 1,
+                strokeOpacity: 1,
+                fillColor: "#0099aa",
+                strokeColor: "#6666aa"
+              }
+            }
+          })
+        ]
+      }),
+      "select": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                pointRadius: 5,
+                graphicName: "square",
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Polygon": {
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                fillColor: "#0000ff",
+                strokeColor: "#0000ff"
+              }
+            }
+          })
+        ]
+      }),
+      "temporary": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                graphicName: "square",
+                pointRadius: 5,
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 2,
+                strokeColor: "#0000ff"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Polygon": {
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff",
+                fillColor: "#0000ff"
+              }
+            }
+          })
+        ]
+      })
+    });
+
+
+    var studyArea = new OpenLayers.Layer.Vector("Study Area Overlay", {
+      strategies: [new OpenLayers.Strategy.Fixed()],                
+      protocol: new OpenLayers.Protocol.HTTP({
+        url: "data/studyAreaLayer.json",
+        format: new OpenLayers.Format.GeoJSON()
+      }),
+      styleMap: styles
+    });
+
+    this.map.addLayer(studyArea);
+
+
+
+    //Focus Areas
+    var focusAreasStyles = new OpenLayers.StyleMap({
+      "default": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                pointRadius: 5,
+                graphicName: "square",
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 1,
+                strokeOpacity: 1,
+                strokeColor: "#3333aa"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#6666aa"
+              },
+              "Polygon": {
+                strokeWidth: 1,
+                strokeOpacity: 1,
+                fillColor: "#0099aa",
+                strokeColor: "#6666aa"
+              }
+            }
+          })
+        ]
+      }),
+      "select": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                pointRadius: 5,
+                graphicName: "square",
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Polygon": {
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                fillColor: "#0000ff",
+                strokeColor: "#0000ff"
+              }
+            }
+          })
+        ]
+      }),
+      "temporary": new OpenLayers.Style(null, {
+        rules: [
+          new OpenLayers.Rule({
+            symbolizer: {
+              "Point": {
+                graphicName: "square",
+                pointRadius: 5,
+                fillColor: "white",
+                fillOpacity: 0.25,
+                strokeWidth: 2,
+                strokeColor: "#0000ff"
+              },
+              "Line": {
+                strokeWidth: 3,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff"
+              },
+              "Polygon": {
+                strokeWidth: 2,
+                strokeOpacity: 1,
+                strokeColor: "#0000ff",
+                fillColor: "bbbb00"
+              }
+            }
+          })
+        ]
+      })
+    });
+
+
+    var focusAreas = new OpenLayers.Layer.Vector("Focus Area Overlay", {
+      strategies: [new OpenLayers.Strategy.Fixed()],                
+      protocol: new OpenLayers.Protocol.HTTP({
+        url: "data/focusAreasLayer.json",
+        format: new OpenLayers.Format.GeoJSON()
+      }),
+      styleMap: focusAreasStyles
+    });
+
+    this.map.addLayer(focusAreas);
+
+
+
 
 
     this.map.setCenter(
